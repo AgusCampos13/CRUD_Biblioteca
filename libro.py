cff6bc76c818cf15dbc80fdea628aecdc7636ba7
Agustina Campos <agustinacamposrivarola@gmail.com>
2025-04-29 23:27:43 -0300

    Agregar Libro

---
+++ libro.py
@@ -0,0 +1,8 @@
+class Libro:
+
+    def __init__(self, titulo, autor, fecha_de_publicacion, isbn, estado):
+        self.titulo = titulo
+        self.autor = autor
+        self.fecha_de_publicacion = fecha_de_publicacion
+        self.isbn = isbn
+        self.estado = estado
